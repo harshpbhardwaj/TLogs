362c3fbba8ce8b572a6c70ea323fb410871887f9
Harsh Pratap <harshpratap@Harshs-MacBook-Air.local>
2024-02-22 22:39:08 +0530

    csrf coockie

--- HexaBlogs/settings.py
+++ HexaBlogs/settings.py
@@ -146,6 +146,9 @@ USE_L10N = True
 
 USE_TZ = False
 
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
